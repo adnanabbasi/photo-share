48f13d32b14b14c88837e8fba364a21e45821b80
adnanabbasi <adnan_abbasi@yahoo.com>
2023-07-05 23:36:39 -0400

    Using MariaDB and Sequalize

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express')
 const app = new express()
-const port = 8080
+const db = require('./models')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 app.use(express.static('public'))
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs')
+
+//db
+const sqlPort = 3307 // 3306 or 3307
+db.sequelize
+  .sync({})
+  .then(() => {
+    app.listen(sqlPort, () => {
+      console.log(`Mariadb Connection Successful - http://localhost:${sqlPort}.`)
+    })
+  })
+  .catch(error => {
+    console.error('Unable to connect to the database', error)
+  })
+//server
+const port = 8080
 app.listen(port, () => {
   console.log(`Serving photo app on http://localhost:${port}`)
 })
 
-app.get("/", (request, response) => {
-  response.render("index")
+// routes
+app.get('/', (request, response) => {
+  response.render('index')
 })
